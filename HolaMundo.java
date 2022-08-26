b18b9a2dea08a9819dd8e52e8e7b1845f4c091a8
Adrian Ceballos <adcom3@hotmail.com>
2022-08-25 19:11:01 -0500

    recfactor: add methods sumar and restar HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,4 +2,16 @@ class HolaMundo{
     public static void main(String[] args){
 
     }
+
+    public static int sumar(int n1, int n2) {
+
+        return n1+n2;
+        
+    }
+    
+    public static int restar(int n1, int n2) {
+    
+        return n1-n2;
+        
+    }
 }
